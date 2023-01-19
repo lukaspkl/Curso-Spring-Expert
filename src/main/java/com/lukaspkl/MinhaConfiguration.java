c84a25d8768d27156639d49da0883b16b7b78dc8
lukaspkl <fohrenoficial@Live.com>
2023-01-19 20:18:13 -0300

    Aula 2 configuration e beans

--- src/main/java/com/lukaspkl/MinhaConfiguration.java
+++ src/main/java/com/lukaspkl/MinhaConfiguration.java
@@ -1,5 +1,13 @@
 package com.lukaspkl;
 
-@org.springframework.context.annotation.Configuration
-public class Configuration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+
+    @Bean(name = "applicationName" )
+    public String applicationName (){
+        return "Sistema de vendas";
+    }
 }
--- src/main/java/com/lukaspkl/VendasApplication.java
+++ src/main/java/com/lukaspkl/VendasApplication.java
@@ -1,5 +1,7 @@
 package com.lukaspkl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationname;
+
     @GetMapping("/hello")
-    public String helloworld(){
-        return "Hello World!";
+    public String helloworld (){
+        return applicationname;
     }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
