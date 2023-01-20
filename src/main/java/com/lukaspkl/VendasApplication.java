2bd17f23696dac19cefc5f0773a69f9cd79298b8
lukaspkl <fohrenoficial@Live.com>
2023-01-19 23:35:17 -0300

    Modulo 3 Persistencia e Acesso a dados com Spring - Aula 14

--- src/main/java/com/lukaspkl/VendasApplication.java
+++ src/main/java/com/lukaspkl/VendasApplication.java
@@ -1,24 +1,14 @@
 package com.lukaspkl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController
+
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
-    private String applicationname;
-
-    @GetMapping("/hello")
-    public String helloworld (){
-        return applicationname;
-    }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
 
--- src/main/java/com/lukaspkl/model/Cliente.java
+++
@@ -1,4 +0,0 @@
-package com.lukaspkl.model;
-
-public class Cliente {
-}
