21379e0a48af41b2796017d95aef1f19489c8c84
lukaspkl <fohrenoficial@Live.com>
2023-01-20 17:16:49 -0300

    Aula 31 Criando Repositorios das Entidades

---
+++ src/main/java/com/lukaspkl/domain/repository/ItemsPedido.java
@@ -0,0 +1,7 @@
+package com.lukaspkl.domain.repository;
+
+import com.lukaspkl.domain.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemsPedido extends JpaRepository<ItemPedido, Integer> {
+}
---
+++ src/main/java/com/lukaspkl/domain/repository/Produtos.java
@@ -0,0 +1,6 @@
+package com.lukaspkl.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Produtos extends JpaRepository<Produtos,Integer> {
+}
