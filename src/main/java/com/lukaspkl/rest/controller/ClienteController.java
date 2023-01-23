af34ccf18189877656958629819b63f486c4484e
lukaspkl <fohrenoficial@Live.com>
2023-01-23 17:26:22 -0300

    Aula 38 Requisição GET com parametros e Response Entity

--- src/main/java/com/lukaspkl/rest/controller/ClienteController.java
+++ src/main/java/com/lukaspkl/rest/controller/ClienteController.java
@@ -1,20 +1,35 @@
 package com.lukaspkl.rest.controller;
 
 
+import com.lukaspkl.domain.entity.Cliente;
+import com.lukaspkl.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public String helloCliente (@PathVariable("nome") String nomeCliente){
-        return String.format("hello %s ",  nomeCliente);
+    public ResponseEntity getclienteById (@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if (cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
+
+
     }
 
 
