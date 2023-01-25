a1944bbeffff98d9392ad4a0b92bb2d2663945bb
lukaspkl <fohrenoficial@Live.com>
2023-01-24 23:54:23 -0300

    Aula 43 @Response status e @Restcontroller : refatorando API

---
+++ src/main/java/com/lukaspkl/rest/ApiErrors.java
@@ -0,0 +1,8 @@
+package com.lukaspkl.rest;
+
+import java.util.List;
+
+public class ApiErrors {
+
+    List<String> errors;
+}
---
+++ src/main/java/com/lukaspkl/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,9 @@
+package com.lukaspkl.rest.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ApplicationControllerAdvice {
+
+
+}
